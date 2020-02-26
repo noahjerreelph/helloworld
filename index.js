ca4d7c89148ab01db3d3550e527e164a5dda1b74
Noah JerreelGuillen <nguillen@village88.com>
2020-02-26 16:00:48 +0800

    adding games dashboard

--- index.js
+++ index.js
@@ -2,28 +2,42 @@ const express 			    = require('express');
 const app 				    = express();
 const http 			        = require('http');
 const server 				= http.Server(app);
+const router 				= express.Router();
 
 const hostname 	= '0.0.0.0';
 const port 		=  process.env.NODE_ENV == "production" ? 9001 : 9002;
 
 
-app.get("/", (req, res) => {
+
+app.use(express.static(__dirname + "/assets"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+router.get("/", (req, res) => {
       res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
 	  res.end('Hello World from Village88!');
 })
 
-app.get("/subscribe", (req, res) => {
+router.get("/subscribe", (req, res) => {
       res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
 	  res.end('Do you want to subscribe to Village88 Projects?');
 })
 
-app.get("/unsubscribe", (req, res) => {
+router.get("/unsubscribe", (req, res) => {
       res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
 	  res.end('Are you sure you want to unsubscribe?');
 })
+
+router.get("/games", (req, res) => {
+     res.render('games');
+})
+
+
+app.use('/', router);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
