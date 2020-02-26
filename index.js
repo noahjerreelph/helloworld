f2b13f91bfd6851d49eae526d43709f6e1f1f4af
Noah JerreelGuillen <nguillen@village88.com>
2020-02-26 13:37:53 +0800

    adding subscription page

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const http = require('http');
+const express 			    = require('express');
+const app 				    = express();
+const http 			        = require('http');
+const server 				= http.Server(app);
 
 const hostname 	= '0.0.0.0';
 const port 		=  process.env.NODE_ENV == "production" ? 9001 : 9002;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Village88!');
-});
+
+app.get("/", (req, res) => {
+      res.statusCode = 200;
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.end('Hello World from Village88!');
+})
+
+app.get("/subscribe", (req, res) => {
+      res.statusCode = 200;
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.end('Are you sure you want to subscribe?');
+})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
